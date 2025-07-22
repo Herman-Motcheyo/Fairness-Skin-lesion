a4f54792a7f2c5d7f0be8c37590c372227ec326e
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2025-07-22 13:24:33 +0100

    plot training function

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 def augment_image(image):
     # Flip horizontal et vertical
@@ -63,3 +65,41 @@ def save_history_to_csv(history, fold_number, models_name, save_dir='histories')
 
     hist_df.to_csv(file_path, index=False)
     print(f"Historique sauvegardé : {file_path}")
+
+
+def plot_training(hist):
+    tr_acc = hist.history['accuracy']
+    tr_loss = hist.history['loss']
+    val_acc = hist.history['val_accuracy']
+    val_loss = hist.history['val_loss']
+    index_loss = np.argmin(val_loss)
+    val_lowest = val_loss[index_loss]
+    index_acc = np.argmax(val_acc)
+    acc_highest = val_acc[index_acc]
+
+    plt.figure(figsize= (20, 8))
+    plt.style.use('fivethirtyeight')
+    Epochs = [i+1 for i in range(len(tr_acc))]
+    loss_label = f'best epoch= {str(index_loss + 1)}'
+    acc_label = f'best epoch= {str(index_acc + 1)}'
+    
+    plt.subplot(1, 2, 1)
+    plt.plot(Epochs, tr_loss, 'r', label= 'Training loss')
+    plt.plot(Epochs, val_loss, 'g', label= 'Validation loss')
+    plt.scatter(index_loss + 1, val_lowest, s= 150, c= 'blue', label= loss_label)
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    
+    plt.subplot(1, 2, 2)
+    plt.plot(Epochs, tr_acc, 'r', label= 'Training Accuracy')
+    plt.plot(Epochs, val_acc, 'g', label= 'Validation Accuracy')
+    plt.scatter(index_acc + 1 , acc_highest, s= 150, c= 'blue', label= acc_label)
+    plt.title('Training and Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    
+    plt.tight_layout
+    plt.show()
